ae17f5d8515b01ca2ff229125c3c6710a5c31273
kenil gor <kenil.a.patel.10@gmail.com>
2024-09-30 01:28:47 +0530

    Utils Code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    colsole.log("Mongo Db Connection Failed: " + err);
+    });
 
 
 
